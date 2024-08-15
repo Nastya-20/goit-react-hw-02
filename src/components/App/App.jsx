6d60f4635bf9394f657d1160c7b4b155d28da39b
Your NameNastya-20 <asyataa2016@gmail.com>
2024-08-15 14:52:29 +0300

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import 'react-dom';
 import Feedback from '../Feedback/Feedback';
